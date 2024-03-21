7e2b6af7d34b121b1b845326c56d19afec396d14
pat-alt <altmeyerpat@gmail.com>
2024-03-21 17:01:33 +0100

    tests passing

--- test/parallelization.jl
+++ test/parallelization.jl
@@ -1,16 +1,13 @@
 using TaijaBase: parallelize, vectorize_collection, ProcessStyle, IsParallel, parallelizable
 
-foo(x) = x^2
-bar(x) = x^2
+ProcessStyle(::Type{<:typeof(sum)}) = IsParallel()
+ProcessStyle(::Type{<:typeof(prod)}) = NotParallel()
 
 @testset "Parallelization" begin
     @test vectorize_collection([1, 2, 3]) == [1, 2, 3]
     @test vectorize_collection([1 2; 3 4]) == [[1 2; 3 4]]
     @test vectorize_collection(zip([1, 2, 3], [4, 5, 6])) == [1, 2, 3]
     @test parallelize(nothing, x -> x^2, [1, 2, 3]) == [1, 4, 9]
-    ProcessStyle(::Type{<:typeof(foo)}) = IsParallel()
-    println(parallelizable(foo))
-    @test parallelizable(foo) == true
-    ProcessStyle(::Type{<:typeof(bar)}) = NotParallel()
-    @test parallelizable(bar) == false
+    @test parallelizable(sum) == true
+    @test parallelizable(Base.product) == false
 end
