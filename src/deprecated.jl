ecb7415397b8c02b8694ada0f5c168ec9047d950
pat-alt <altmeyerpat@gmail.com>
2024-04-05 14:29:43 +0200

    deprecated wrong function

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,4 +1,4 @@
 Base.@deprecate_moved ProcessStyle "TaijaParallel"
 Base.@deprecate_moved NotParallel "TaijaParallel"
 Base.@deprecate_moved IsParallel "TaijaParallel"
-Base.@deprecate_moved parallelize "TaijaParallel"
+Base.@deprecate_moved parallelizable "TaijaParallel"
